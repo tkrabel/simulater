1475a26c568ab4af60eb3792c890549035afc2e6
tkrabel <tobiaskrabel@gmx.de>
2018-06-12 12:13:17 +0200

    INIT devstuff

--- data-raw/devstuff.R
+++ data-raw/devstuff.R
@@ -5,3 +5,10 @@
 use_testthat()
 use_test("simulater.R")
 
+source("R/utils.R")
+source("R/simulater.R")
+
+simulation <- simulater(2e5, 10, 15, 10, n_noise = 10, stn = 1,
+                        funs = list(sin = function(x) sin(x),
+                                    exp = function(x) exp(x)))
+simulation$formula
